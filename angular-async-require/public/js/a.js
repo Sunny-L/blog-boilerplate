ad2924d1c6c25f3bdeb7f26060260dc07dc02d1c
sunny <lixu158@sina.cn>
2016-07-21 17:39:58 +0800

    angular-async-requirejs

---
+++ angular-async-require/public/js/a.js
@@ -0,0 +1,5 @@
+define([],function(){
+    app.register.controller('aCtrl',function($scope){
+        $scope.text = 'hello I am A!';
+    })
+})
---
+++ angular-async-require/public/js/b.js
@@ -0,0 +1,5 @@
+define([],function(){
+    app.register.controller('bCtrl',function($scope){
+        $scope.text = 'hello I am B!';
+    })
+})
